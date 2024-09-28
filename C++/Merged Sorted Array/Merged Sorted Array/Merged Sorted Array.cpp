8259c309195d7af01e10256f006edc9b8bfb7d79
Genesis Jackson <resiztdzn@gmail.com>
2024-09-27 22:31:04 -0400

    Improved coding for leet code 88

--- C++/Merged Sorted Array/Merged Sorted Array/Merged Sorted Array.cpp
+++ C++/Merged Sorted Array/Merged Sorted Array/Merged Sorted Array.cpp
@@ -1,16 +1,13 @@
 // Leet Code: 88. Merg Soreted Array
 // Language: C++
 
-class Solution {
+class Solution
+{
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> JJKMerger;
-        for (int i = 0; i < m; i++)
-            JJKMerger.push_back(nums1[i]), JJKMerger.push_back(nums2[i]);
-
-        sort(JJKMerger.begin(), JJKMerger.end());
-
-        for (int i = 0; i < nums1.size(); i++)
-            nums1[i] = JJKMerger[i];
+    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
+    {
+        for (int i = 0; i + m < nums1.size(); i++)
+            nums1[i + n] = nums2[i];
+        sort(nums1.begin(), nums1.end());
     }
 };
